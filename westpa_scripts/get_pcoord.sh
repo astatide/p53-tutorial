4155a91f0372fbb1a05c9f10e3d26837600d171e
ajpratt <ajp105@pitt.edu>
2015-07-01 15:27:19 -0400

    Updated get_pcoord.sh from na_cl to the p53.

--- westpa_scripts/get_pcoord.sh
+++ westpa_scripts/get_pcoord.sh
@@ -7,27 +7,46 @@ fi
 
 cd $WEST_SIM_ROOT
 
-DIST=_nacldist_$$.xvg
+DIST=_p53dist_$$.xvg
+DIST_OUT=_p53dist_$$.out.xvg
+RMSD=_p53dist_$$.xvg
+RMSD_OUT=_p53dist_$$.out.xvg
 
 function cleanup() {
-    rm -f $DIST
+    rm -f $DIST $RMSD
 }
 
 trap cleanup EXIT
 
 # Get progress coordinate
 if [ ${G_DIST} ]; then
-    # For GROMACS 4, use g_dist
-    COMMAND="2 \n 3 \n"
-    echo -e $COMMAND | $G_DIST -f $WEST_STRUCT_DATA_REF \
-      -s $WEST_SIM_ROOT/gromacs_config/nacl.tpr -o $DIST
-    cat $DIST | tail -n +23 | awk '{print $2*10;}' > $WEST_PCOORD_RETURN
-elif [ ${GMX} ]; then
-    # For GROMACS 5, use gmx distance
-    $GMX distance -f $WEST_STRUCT_DATA_REF  -s $WEST_STRUCT_DATA_REF \
-      -oall $DIST -select 1
-    cat $DIST | tail -n +16 | awk '{print $2*10;}' > $WEST_PCOORD_RETURN
-fi
+    # For GROMACS 4, use trjconv, g_rms, and g_dist.
+    # Currently, GROMACS 5 isn't supported.
+
+    # Generate a .tpr file for use in g_dist and g_rms
+    $GROMPP -f md.mdp -c $WEST_STRUCT_DATA_REF.gro -p $TOP \
+          -t $WEST_STRUCT_DATA_REF.trr -o $WEST_STRUCT_DATA_REF.tpr -po md_out.mdp -n $NDX
+    rm md_out.mdp
+
+    # Update the command, then calculate the first dimension of the progress coordinate: end to end distance.
+    COMMAND="18 \n 19 \n"
+    echo -e $COMMAND \
+      | $G_DIST -f $WEST_STRUCT_DATA_REF.gro -s $WEST_STRUCT_DATA_REF.tpr -o $DIST -xvg none -ndx $NDX || exit 1
+    cat $DIST | awk '{print $2*10;}' > $DIST_OUT
+
+    # Update the command again, then run g_rms to calculate to second the dimension: the heavy atom rmsd of the protein aligned on itself.
+    COMMAND="2 \n 2 \n"
+    echo -e $COMMAND \
+      | $G_RMS -s $REF_LOC -f $WEST_STRUCT_DATA_REF.gro -n $NDX -xvg none || exit 1
+    cat $RMSD_OUT | awk '{print $2*10;}' > $RMSD_OUT
+
+    # Return the true pcoord.
+    paste $DIST_OUT $RMSD_OUT > $WEST_PCOORD_RETURN || exit 
+
+    # Remove the imaged trajectories we created that we don't need, anymore, as these are not likely to
+    # be caught in the cleanup step (we don't want to copy them off scratch), as well as the reference file.
+
+    rm -f $DIST $DIST_OUT $RMSD $RMSD_OUT
 
 if [ -n "$SEG_DEBUG" ] ; then
     head -v $WEST_PCOORD_RETURN
