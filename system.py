c890873aa6c12cbf0360a300bd247abf8ca43208
ajpratt <ajp105@pitt.edu>
2015-07-01 15:09:47 -0400

    Added in the preliminary code for the color state tracker; hopefully pulled the correct version (i.e., the one that has been pre bug checked and works).  Added comments and made them in line with numpy's docstrings.

--- system.py
+++ system.py
@@ -11,14 +11,14 @@ log.debug('loading module %r' % __name__)
 
 class System(WESTSystem):
     """
-    System for sodium chloride association
+    System for P53 folding and unfolding.
     """
 
     def initialize(self):
         """
         Initializes system
         """
-        self.pcoord_ndim  = 1
+        self.pcoord_ndim  = 3
         self.pcoord_len   = 6
         self.pcoord_dtype = numpy.float32
         binbounds         = [ 0.00, 2.80, 2.88, 3.00, 3.10, 3.29, 3.79, 3.94,
@@ -28,7 +28,68 @@ class System(WESTSystem):
 
         self.bin_target_counts      = numpy.empty((self.bin_mapper.nbins,),
                                         numpy.int)
-        self.bin_target_counts[...] = 24
+        self.bin_target_counts[...] = 4
+
+def _2D_pcoord_loader_color_tracker(fieldname, coord_file, segment, single_point=False):
+    """
+    This function loads a 2-dimensional progress coordinate, performs some logic to track color,
+    then returns the 2+1 dimensional progress coordinate to the system to be processed.
+    In this tutorial, there are 3 dimensions specified in this file; runseg.sh returns two of them.
+    The third is calculated here.
+    Note that we are defining our states only based on one progress coordinate dimension, in this example.
+
+    **Arguments:**
+        :*fieldname*:      Key at which to store dataset
+        :*coord_filename*: Temporary file from which to load coordinates
+        :*segment*:        WEST segment
+        :*single_point*:   Data to be stored for a single frame
+                           (only false half the time)
+    """
+
+    # These are the raw coordinates.
+    coord_raw = numpy.loadtxt(coord_file, dtype=numpy.float32) 
+    # These are the states; they are left inclusive, and right exclusive, which is consistent with the normal
+    # binning procedure.
+    color_bins = [(0.0,0.20),(11.60,float('inf'))]
+    unknown_state = 2
+    system = westpa.rc.get_system_driver()
+
+    if single_point == True:
+        npts = 1
+    else:
+        npts = system.pcoord_len
+
+    coords = numpy.empty((npts,2), numpy.float32)
+    colors = numpy.empty((npts), numpy.float32)
+    if single_point == True:
+        colors[:] = unknown_state
+        for istate,state_tuple in enumerate(color_bins):
+            # Note that here, we are using the first dimension and first dimension alone.
+            # The shape of the returned coord_raw is slightly different if single_point evalues
+            # to true.
+            # We evalulate whether or not we're in a state; if not, we leave it as in the
+            # unknown state.
+            if coord_raw[0] >= state_tuple[0] and coord_raw[0] < state_tuple[1]:
+                colors[:] = istate
+        coords[:] = coord_raw[...]
+    else:
+        # If we're not the first point, we set the state to be what it was in the beginning
+        # of the iteration.  We only want to update the state when we update a bin for purposes
+        # of state tracking.
+        colors[:] = segment.pcoord[0][2]
+        coords[:] = coord_raw[...]
+
+    for istate,state_tuple in enumerate(color_bins):
+        if coords[-1,0] >= state_tuple[0] and coords[-1,0] < state_tuple[1]:
+            colors[-1] = istate
+    
+    # We require different stacking behavior to return things in the proper order
+    # depending on how many points we have.  I could probably clean this up.
+    if single_point == True:
+        segment.pcoord = numpy.hstack((coords[0,0],coords[0,1],colors[:]))
+    else:
+        # This could easily be modified to return N dimensions.
+        segment.pcoord = numpy.swapaxes(numpy.vstack((coords[:,0],coords[:,1],colors[:])), 0, 1)
 
 def coord_loader(fieldname, coord_filename, segment, single_point=False):
     """
