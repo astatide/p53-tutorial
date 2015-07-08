f89ac49c8a8e27aac3e4cb92332f1252941507e6
ajpratt <ajoshpratt@gmail.com>
2015-07-08 15:11:21 -0400

    Adjusted submission script; removed position restraints from mdp file.

--- runwe_frank.sh
+++ runwe_frank.sh
@@ -3,7 +3,7 @@
 #PBS -S /bin/bash
 #PBS -j oe
 #PBS -l walltime=24:00:00
-#PBS -l nodes=1:ppn=32
+#PBS -l nodes=1:ppn=64
 #PBS -q dist_amd
 #PBS -m ae
 #PBS -A westpa2015
@@ -14,26 +14,6 @@ source env.sh || exit 1
 
 cd $WEST_SIM_ROOT
 
-# start server in processes mode.
 $WEST_ROOT/bin/w_run --work-manager=processes --n-workers=$PBS_NUM_PPN &> west-$PBS_JOBID.log &
-
-# wait on host info file up to one minute
-#for ((n=0; n<60; n++)); do
-#    if [ -e $SERVER_INFO ] ; then
-#        echo "== server info file $SERVER_INFO =="
-#        cat $SERVER_INFO
-#        break
-#    fi
-#    sleep 1
-#done
-
-# exit if host info file doesn't appear in one minute
-#if ! [ -e $SERVER_INFO ] ; then
-#    echo 'server failed to start'
-#    exit 1
-#fi
-
-# start clients, with the proper number of cores on each
-#pbsdsh -v -u $PWD/node.sh --work-manager=zmq --zmq-mode=client --n-workers=32 --zmq-info=$SERVER_INFO &
 
 wait
